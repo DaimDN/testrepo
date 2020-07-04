3c76af55d041e2c876c04c3aca8a84b4a4489891
unknown <you@example.com>
2020-07-04 14:45:15 +0100

    todat added the files

---
+++ daim.js
@@ -0,0 +1,48 @@
+var input1 = prompt("Enter number 1 ");
+var input2 = prompt("Enter number 2 ");
+
+var operator = prompt("please select a mathimtaical operator either +, -, /, *");
+
+
+switch(operator){
+
+    case "+":
+
+        var result = parseInt(input1) + parseInt(input2);
+
+        alert(result);
+        break;
+
+        case "-":
+            var result = parseInt(input1) - parseInt(input2);
+
+alert(result);
+            break;
+
+            case "*" :
+                var result = parseInt(input1) * parseInt(input2);
+
+alert(result);
+                break;
+
+                case "/":
+                    var result = parseInt(input1) / parseInt(input2);
+
+alert(result);
+
+                break;
+
+                default : alert("User Syntax Error");
+}
+
+
+
+
+
+
+
+
+
+
+
+
